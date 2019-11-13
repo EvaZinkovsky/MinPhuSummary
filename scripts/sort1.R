8acbc597074b271bcd31c4fbb7f6b77f83dcc382
Zinkovsky, Eva (A&F, Black Mountain) <eva.zinkovsky@csiro.au>
2019-11-13 12:35:40 +1100

    repeat for domestic and export sales

--- scripts/sort1.R
+++ scripts/sort1.R
@@ -7,71 +7,38 @@ library(lubridate)
 # find the names of the sheets contained in the excel dataset        
 excel_sheets("data/MinhPhuSales_09112019.xlsx")
 
-# create a table of the internal sales for 2018
 internal2018 <- read_excel("data/MinhPhuSales_09112019.xlsx", 1)
-
-# remove all the spaces in the column names
 internal2018 <- select(internal2018, "DateOfShipment" = "Date of Shipment", "Destination", "SecondProductCategory" = "2nd product category", "ThirdProductCategory" = "3rd product category", "QuantityPackages" = "Quantity (Packages)", "QuantityKG" = "Quanity (KG)","Plant", "PlantName" = "Plant Name", "Certificate", "FirstLevelExportMarket" = "1st level export market", "SecondLevelExportMarket" = "2nd level export market")
-
-# create a table of the internal sales for 2019
 internal2019 <- read_excel("data/MinhPhuSales_09112019.xlsx", 4)
-
-# remove all the spaces in the column names
 internal2019 <- select(internal2019, "DateOfShipment" = "Date of Shipment", "Destination", "SecondProductCategory" = "2nd product category", "ThirdProductCategory" = "3rd product category", "QuantityPackages" = "Quantity (Packages)", "QuantityKG" = "Quanity (KG)","Plant", "PlantName" = "Plant Name", "Certificate", "FirstLevelExportMarket" = "1st level export market", "SecondLevelExportMarket" = "2nd level export market")
-
-# combine internal2018 and internal2019 into internal1819
 internal1819 <- bind_rows(internal2018, internal2019)
 
-# create a new column which has the month and year of each sale only
 internal1819$ShipmentMonth <- format(as.Date(internal1819$DateOfShipment), "%Y-%m") 
 
-# find the quantity of each product sold each month in each destination 
 internalbymonth <- internal1819 %>% 
   group_by(ShipmentMonth, Destination, SecondProductCategory)%>%
   summarise(QuantityKG = sum(QuantityKG))
- 
-# create a table of the domestic sales for 2018            
+             
 domestic2018 <- read_excel("data/MinhPhuSales_09112019.xlsx", 2)
-
-# remove all the spaces in the column names
 domestic2018 <- select(domestic2018, "DateOfShipment" = "Date of Shipment", "Destination", "SecondProductCategory" = "2nd product category", "ThirdProductCategory" = "3rd product category", "QuantityPackages" = "Quantity (Packages)", "QuantityKG" = "Quanity (KG)","Plant", "PlantName" = "Plant Name", "Certificate", "FirstLevelExportMarket" = "1st level export market", "SecondLevelExportMarket" = "2nd level export market")
-
-# create a table of the domestic sales for 2019
 domestic2019 <- read_excel("data/MinhPhuSales_09112019.xlsx", 5)
-
-# remove all the spaces in the column names
 domestic2019 <- select(domestic2019, "DateOfShipment" = "Date of Shipment", "Destination", "SecondProductCategory" = "2nd product category", "ThirdProductCategory" = "3rd product category", "QuantityPackages" = "Quantity (Packages)", "QuantityKG" = "Quanity (KG)","Plant", "PlantName" = "Plant Name", "Certificate", "FirstLevelExportMarket" = "1st level export market", "SecondLevelExportMarket" = "2nd level export market")
-
-# combine domestic2018 and domestic2019 into domestic1819
 domestic1819 <- bind_rows(domestic2018, domestic2019)
 
-# create a new column which has the month and year of each sale only
 domestic1819$ShipmentMonth <- format(as.Date(domestic1819$DateOfShipment), "%Y-%m") 
 
-# find the quantity of each product sold each month in each destination
 domesticbymonth <- domestic1819 %>% 
   group_by(ShipmentMonth, Destination, SecondProductCategory)%>%
   summarise(QuantityKG = sum(QuantityKG))
 
-# create a table of the export sales for 2018
 export2018 <- read_excel("data/MinhPhuSales_09112019.xlsx", 3)
-
-# remove all the spaces in the column names
 export2018 <- select(export2018, "DateOfShipment" = "Date of Shipment", "Destination", "SecondProductCategory" = "2nd product category", "ThirdProductCategory" = "3rd product category", "QuantityPackages" = "Quantity (Packages)", "QuantityKG" = "Quanity (KG)","Plant", "PlantName" = "Plant Name", "Certificate", "FirstLevelExportMarket" = "1st level export market", "SecondLevelExportMarket" = "2nd level export market")
-
-# create a table of the export sales for 2019 
 export2019 <- read_excel("data/MinhPhuSales_09112019.xlsx", 6)
-
-# remove all the spaces in the column names
 export2019 <- select(export2019, "DateOfShipment" = "Date of Shipment", "Destination", "SecondProductCategory" = "2nd product category", "ThirdProductCategory" = "3rd product category", "QuantityPackages" = "Quantity (Packages)", "QuantityKG" = "Quanity (KG)","Plant", "PlantName" = "Plant Name", "Certificate", "FirstLevelExportMarket" = "1st level export market", "SecondLevelExportMarket" = "2nd level export market")
-
-# combine export2018 and export2019 into export1819
 export1819 <- bind_rows(export2018, export2019)
 
-# create a new column which has the month and year of each sale only
 export1819$ShipmentMonth <- format(as.Date(export1819$DateOfShipment), "%Y-%m") 
 
-# find the quantity of each product sold each month in each destination
 exportbymonth <- internal1819 %>% 
   group_by(ShipmentMonth, Destination, SecondProductCategory)%>%
   summarise(QuantityKG = sum(QuantityKG))
